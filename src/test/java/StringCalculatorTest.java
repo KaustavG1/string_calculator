a5b14a51094750c32fbbdd537fa9922929117596
KaustavG1 <mail2kg2@gmail.com>
2021-02-24 11:13:01 +0530

    Test added for empty string

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -5,8 +6,11 @@ import static org.junit.jupiter.api.Assertions.*;
 class StringCalculatorTest {
 
     @Test
+    @DisplayName("Result should be 0 if empty string is passed as the argument")
     void add() {
         StringCalculator stringCalculator = new StringCalculator();
-
+        int actualResult = stringCalculator.add("");
+        int expectedResult = 0;
+        assertEquals(actualResult, expectedResult);
     }
 }
