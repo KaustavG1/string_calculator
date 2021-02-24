261f7d312fd3500e674f9b3066e8eb52a01a88e1
KaustavG1 <mail2kg2@gmail.com>
2021-02-24 12:06:35 +0530

    Logic added for single argument test to pass

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,10 +1,11 @@
+import java.util.regex.Pattern;
+
 public class StringCalculator {
     public int add(String numbers) {
         if(numbers.equals("")) {
             return 0;
-        } else if(true) {
-            // ToDo: Implement Logic to make test case pass
-            return 1;
+        } else if(Pattern.matches("[0-9]+", numbers)) {
+            return Integer.parseInt(numbers);
         } else {
             // ToDo: Implement Logic to make test case pass
             return 0;
