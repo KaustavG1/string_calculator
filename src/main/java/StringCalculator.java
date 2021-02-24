13714702cbe22be1c9650ad751a328d2ab81fa10
KaustavG1 <mail2kg2@gmail.com>
2021-02-24 22:14:50 +0530

    Code passed New Line test case

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -7,6 +7,7 @@ public class StringCalculator {
         } else if(Pattern.matches("[0-9]+", numbers)) {
             return Integer.parseInt(numbers);
         } else {
+            numbers = numbers.replace("\n", ",");
             String arr[] = numbers.split(",");
             int result = 0;
             for(String a : arr) {
